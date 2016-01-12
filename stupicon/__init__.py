9584c8d3dfde05edbbe5f0a5dccf6a050ea54a14
Robin Gloster <robin@loc-com.de>
2016-01-12 11:02:07 +0000

    send note off/on rising/falling edge

--- stupicon/__init__.py
+++ stupicon/__init__.py
@@ -1,4 +1,3 @@
-import asyncio
 import time
 import logging
 import pygame
@@ -14,14 +13,16 @@ GPIO_PINS = list(range(2, 26))
 class Stupicon:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.loop = asyncio.get_event_loop()
 
         pygame.midi.init()
         self.player = pygame.midi.Output(2)
         self.player.set_instrument(48, 1)
 
         self.setup_gpio()
-        #self.loop.run_forever()
+        # RPIO.setup(12, RPIO.OUT)
+        # RPIO.output(12, True)
+        # time.sleep(1)
+        # RPIO.output(12, False)
         RPIO.wait_for_interrupts()
 
     def setup_gpio(self):
@@ -31,23 +32,26 @@ class Stupicon:
 
     def gpio_callback(self, gpio_id, val):
         self.logger.info('callback {} {}'.format(gpio_id, val))
-        if val == 1:
-            self.play(gpio_id)
+        self.play(gpio_id, not val)
 
     def setup_input_pin(self, pin):
         self.logger.info('setup pin: {}'.format(pin))
-        RPIO.setup(pin, RPIO.IN, pull_up_down=RPIO.PUD_UP)
+        RPIO.setup(pin, RPIO.IN, pull_up_down=RPIO.PUD_DOWN)
 
     def set_interrupt(self, pin):
         self.logger.info('setup interrupt: {}'.format(pin))
-        RPIO.add_interrupt_callback(pin, self.gpio_callback)
+        RPIO.add_interrupt_callback(pin, self.gpio_callback,
+                debounce_timeout_ms=50)
 
-    def play(self, note):
-        self.logger.info('playing: {}'.format(note))
-        self.player.note_on(note, 127, 1)
-        time.sleep(0.1)
-        self.player.note_off(note, 127, 1)
-        # self.loop.call_later(0.1, self.player.note_off(note, 127, 1))
+    def play(self, note, state):
+        self.logger.info('playing: {} {}'.format(
+            note,
+            'on' if state else 'off'
+        ))
+        if state:
+            self.player.note_on(note, 127, 1)
+        else:
+            self.player.note_off(note, 127, 1)
 
 def main():
     try:
